519c137adc9215cb3bfe1d3a3e5094aab3a008f3
Kumar Karikay <krkartikay@gmail.com>
2019-06-12 22:29:50 +0530

    improved script, 3d

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ var svg,
 
 var graph = [], degrees = [];
 
-var k = 0.1;
-var g = 500;
-var d = 0.005;
-var delta = 0.005;
+var k = 10;
+var g = 5e6;
+var d = 2;
+var delta = 0.002;
+
+function randomise_vel(fac){
+    for (var i = 0; i < N; i++){
+        velocities[i][0] += fac * (Math.random() - 0.5);
+        velocities[i][1] += fac * (Math.random() - 0.5);
+        velocities[i][2] += fac * (Math.random() - 0.5);
+    }
+}
 
 // ------------- init ---------------
 
@@ -30,8 +38,8 @@ function init() {
     svg = SVG("draw");
     N = data.nodes.length;
     for (let i = 0; i < data.nodes.length; i++) {
-        locations.push([100 * (Math.random() - .5), 100 * (Math.random() - .5)]);
-        velocities.push([10 * (Math.random() - .5), 10 * (Math.random() - .5)]);
+        locations.push([200 * (Math.random() - .5), 200 * (Math.random() - .5), 200 * (Math.random() - .5)]);
+        velocities.push([0,0,0]);
     }
     for (var [x, y] of data.edges) {
         if (!graph[x]) graph[x] = [];
@@ -47,37 +55,46 @@ function init() {
 
 // ----------- physics -------------
 
+var temp = 1000;
+
 function physics() {
+    randomise_vel(temp/50);
+    temp *= 0.999;
     var new_locations = [];
     var new_velocities = [];
     for (var i = 0; i < N; i++) {
-        var x0 = locations[i][0], y0 = locations[i][1];
-        var vx0 = velocities[i][0], vy0 = velocities[i][1];
+        var x0 = locations[i][0], y0 = locations[i][1], z0 = locations[i][2];
+        var vx0 = velocities[i][0], vy0 = velocities[i][1], vz0 = velocities[i][2];
         for (var j = 0; j < N; j++) {
             // no self-force
             if (i == j) { continue; }
             // gravity
-            var x1 = locations[j][0], y1 = locations[j][1];
-            var vx1 = velocities[j][0], vy1 = velocities[j][1];
-            var dist = Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+            var x1 = locations[j][0], y1 = locations[j][1], z1 = locations[j][2];
+            var vx1 = velocities[j][0], vy1 = velocities[j][1], vz1 = velocities[j][2];
+            var dist = Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2) + Math.pow((z1 - z0), 2));
             var force_x = - g * (x1 - x0) / Math.pow(dist, 3);
             var force_y = - g * (y1 - y0) / Math.pow(dist, 3);
+            var force_z = - g * (z1 - z0) / Math.pow(dist, 3);
             // spring
             if (graph[i] && graph[i][j]) {
                 force_x += k * (x1 - x0);
                 force_y += k * (y1 - y0);
+                force_z += k * (z1 - z0);
             }
             // damping
             force_x += - d * vx0;
             force_y += - d * vy0;
+            force_z += - d * vz0;
             // update (newton's law)
             vx0 += force_x * delta;
             vy0 += force_y * delta;
+            vz0 += force_z * delta;
             x0 += vx0 * delta;
             y0 += vy0 * delta;
+            z0 += vz0 * delta;
         }
-        new_locations.push([x0, y0]);
-        new_velocities.push([vx0, vy0]);
+        new_locations.push([x0, y0, z0]);
+        new_velocities.push([vx0, vy0, vz0]);
     }
     locations = new_locations;
     velocities = new_velocities;
@@ -87,25 +104,43 @@ function physics() {
 
 // ------------- draw --------------
 
+var d1 = 0, d2 = 0, d3 = 0;
 
 function draw() {
     svg.clear();
+    d1 += 0.002;
+    d2 += 0.003;
+    d3 += 0.005;
     // center-of-mass coordinates
-    var cmx = 0, cmy = 0;
-    for (const [x, y] of locations) {
+    var cmx = 0, cmy = 0, cmz = 0;
+    for (const [x, y, z] of locations) {
         cmx += x;
         cmy += y;
+        cmz += z;
     }
-    cmx /= N; cmy /= N;
+    cmx /= N; cmy /= N; cmz /= N;
+    const s3 = Math.sqrt(3)
     for (var i = 0; i < N; i++) {
-        var [x,y] = locations[i];
-        draw_node(20, x - cmx, y - cmy, data.nodes[i]);
+        var [x,y,z] = locations[i];
+        x -= cmx; y -= cmy; z -= cmz;
+        [x,y] = project(x,y,z);
+        draw_node(20, x, y, data.nodes[i]);
     }
     for (const [u, v] of data.edges) {
-        var [x1,y1] = locations[u];
-        var [x2,y2] = locations[v];
-        draw_line(x1-cmx, y1-cmy, x2-cmx, y2-cmy);
+        var [x1,y1,z1] = locations[u];
+        x1 -= cmx; y1 -= cmy; z1 -= cmz;
+        var [x2,y2,z2] = locations[v];
+        x2 -= cmx; y2 -= cmy; z2 -= cmz;
+        [x1, y1] = project(x1, y1, z1);
+        [x2, y2] = project(x2, y2, z2);
+        draw_line(x1, y1, x2, y2);
     }
+}
+
+function project(x,y,z){
+    var A = x*Math.sin(d1) + y*Math.sin(4*Math.PI/3+d2) + z*Math.sin(2*Math.PI/3+d3);
+    var B = x*Math.cos(d1) + y*Math.cos(4*Math.PI/3+d2) + z*Math.cos(2*Math.PI/3+d3);
+    return [A,B];
 }
 
 function draw_node(r, x, y, text) {
@@ -113,12 +148,12 @@ function draw_node(r, x, y, text) {
         stop.at(0, '#0f0', 1.0)
         stop.at(1, '#0f0', 0.0)
     })
-    svg.circle(r * 1.5).center(X(x), Y(y)).fill(gradient);
+    svg.circle(r * 1.5 * 200/scale).center(X(x), Y(y)).fill(gradient);
     svg.circle(r).center(X(x), Y(y)).attr({
         fill: "#111",
         opacity: 0.8,
         stroke: "#0f0",
-        "stroke-width": "2px"
+        "stroke-width": 2 * 200 / scale
     });
     svg.text(text).center(X(x)+30, Y(y)-20).attr({
         fill: "#fff",
@@ -129,19 +164,21 @@ function draw_node(r, x, y, text) {
 
 function draw_line(x1,y1,x2,y2) {
     svg.line(X(x1), Y(y1), X(x2), Y(y2)).stroke({
-        width: 2,
-        opacity: 0.2,
+        width: 2 * 200 / scale,
+        opacity: 0.1,
         color: "#0f0",
         linecap: "round"
     });
 }
 
+var scale = 200;
+
 function X(x) {
-    return width / 2 + x * (Math.min(height, width) / 200);
+    return width / 2 + x * (Math.min(height, width) / scale);
 }
 
 function Y(y) {
-    return height / 2 - y * (Math.min(height, width) / 200);
+    return height / 2 - y * (Math.min(height, width) / scale);
 }
 
 // ------------ animate --------------
--- wiki.py
+++ wiki.py
@@ -5,22 +5,40 @@ import pickle
 from lxml import html
 from collections import deque, defaultdict
 
-TOPIC = "Physics"
+TOPIC = "Mathematics"
+importance = 50
+pages_get = 200
 
-bfs_queue = defaultdict(int)
-bfs_queue[TOPIC] += 1
-
-pages_collected = set()
-all_topics = list([TOPIC])
-topic_count = defaultdict(int)
-links = defaultdict(list)
+try:
+    progress = pickle.load(open("progress.pickle", "rb"))
+    pages_collected = pickle.load(open("pages_collected.pickle", "rb"))
+    all_topics = pickle.load(open("all_topics.pickle", "rb"))
+    links = pickle.load(open("links.pickle", "rb"))
+    topic_count = pickle.load(open("topic_count.pickle", "rb"))
+    bfs_queue = pickle.load(open("bfs_queue.pickle", "rb"))
+except FileNotFoundError:
+    progress = 0
+    pages_collected = set()
+    all_topics = list([TOPIC])
+    links = defaultdict(list)
+    topic_count = defaultdict(int)
+    bfs_queue = defaultdict(int)
+    bfs_queue[TOPIC] += 1
 
 def name(x):
-    return x.replace("_", " ").replace("%E2%80%93", "-").replace("%27","'")
+    return (x.split("(")[0]
+            .replace("_", " ")
+            .replace("%E2%80%93", "-")
+            .replace("%27","'")
+            .strip())
 
 def save_json():
-    topics = [x for (x,y) in topic_count.items() if y>=15]
-    print("Hot topics:",topics,len(topics))
+    topics = [x for (x,y) in topic_count.items() if y>=importance]
+    print("Top topics: (%d)" % len(topics))
+    top = [(name(x), y) for (x, y) in topic_count.items()]
+    top.sort(key = lambda x: -x[1])
+    for t in top[:10]:
+        print(f" - {t[0]} ({t[1]})")
     nodes = [name(x) for x in topics]
     edges = []
     for a in links:
@@ -40,12 +58,10 @@ def save_json():
 def discard(topic):
     pass
 
-progress = 0
-
-while progress < 1000:
+while progress < pages_get:
         topic = max(bfs_queue, key=lambda x: bfs_queue[x])
         bfs_queue.pop(topic)
-        print("Getting:", topic,f"\t(Progress: {progress/10}%)")
+        print("Getting:", name(topic),f"\t(Progress: {progress*100//pages_get}%)")
         if topic in pages_collected:
             continue
         done = False
@@ -66,10 +82,8 @@ while progress < 1000:
         with open(f"wikidmp/{name(topic)}.txt","w") as f:
             f.write(tree.xpath("// *[@id=\"mw-content-text\"]")[0].text_content())
         for i in range(len(ps)):
-            if ps[i].text_content().strip() == '':
-                continue
             for _, _, l, _ in ps[i].iterlinks():
-                if l.count("#") > 0 or len(l.split('/')) != 3:
+                if l.count("#") > 0 or l.count(":") or len(l.split('/')) != 3:
                     continue
                 new_topic = l.split('/')[2]
                 bfs_queue[new_topic] += 1
@@ -77,9 +91,17 @@ while progress < 1000:
                 topic_count[new_topic] += 1
                 if new_topic not in all_topics:
                     all_topics += [new_topic]
-            break
-        print("Next in queue:", sorted(bfs_queue.items(), key=lambda x:-x[1])[:5])
+        next_q = [(name(x[0]),x[1]) for x in sorted(bfs_queue.items(), key=lambda x: -x[1])[:10]]
+        print("Next in queue:")
+        for t in next_q:
+            print(f" - {t[0]} ({t[1]})")
+        pickle.dump(progress, open("progress.pickle", "wb"))
+        pickle.dump(pages_collected, open("pages_collected.pickle", "wb"))
         pickle.dump(links, open("links.pickle", "wb"))
         pickle.dump(topic_count, open("topic_count.pickle", "wb"))
+        pickle.dump(bfs_queue, open("bfs_queue.pickle", "wb"))
+        pickle.dump(all_topics, open("all_topics.pickle", "wb"))
         save_json()
         print("")
+
+save_json()
